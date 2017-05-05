0e3d2ae48a8e4ecc834596085d659ffed013c6f4
wl00887404 <wl00887404@gmail.com>
2017-05-05 23:15:25 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,25 +2,25 @@ Reveal.initialize({
     //history: true,
     // More info https://github.com/hakimel/reveal.js#dependencies 
     transition: 'convex', // none/fade/slide/convex/concave/zoom
-    //autoSlide: 8700,
-    dependencies: [{
-            src: 'plugin/markdown/marked.js'
-        },
-        {
-            src: 'plugin/markdown/markdown.js'
-        },
-        {
-            src: 'plugin/notes/notes.js',
-            async: true
-        },
-        {
-            src: 'plugin/highlight/highlight.js',
-            async: true,
-            callback: function() {
-                hljs.initHighlightingOnLoad();
-            }
-        }
-    ]
+    // autoSlide: 8700,
+    // dependencies: [{
+    //         src: 'plugin/markdown/marked.js'
+    //     },
+    //     {
+    //         src: 'plugin/markdown/markdown.js'
+    //     },
+    //     {
+    //         src: 'plugin/notes/notes.js',
+    //         async: true
+    //     },
+    //     {
+    //         src: 'plugin/highlight/highlight.js',
+    //         async: true,
+    //         callback: function() {
+    //             hljs.initHighlightingOnLoad();
+    //         }
+    //     }
+    // ]
 });
 particlesJS.load('particles-js', 'particlesjs-config.json', function() {
     console.log('callback - particles.js config loaded');
